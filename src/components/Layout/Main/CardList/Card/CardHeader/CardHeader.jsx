4e6686db222ae25a75718adef9777a9abdac322f
Aliaksandr Matveichykau <AMatveychykau@gomel.iba.by>
2021-03-18 12:44:57 +0300

    eslint fix

--- src/components/Layout/Main/CardList/Card/CardHeader/CardHeader.jsx
+++ src/components/Layout/Main/CardList/Card/CardHeader/CardHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './CardHeader.css';
 
-const CardHeader = ({title}) => {
+const CardHeader = ({ title }) => {
     return <div className="cardHeader">{title}</div>;
 };
 
